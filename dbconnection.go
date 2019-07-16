3edae9ed44837a761153b5d631212cb250d2b928
ChaitanyaAkula <krishchaituakula@gmail.com>
2019-07-16 15:27:56 +0530

    Add files via upload

---
+++ dbconnection.go
@@ -0,0 +1,17 @@
+package dbconnection
+import(
+	"log"
+	_"github.com/go-sql-driver/mysql"
+	//_"mysqlserver"
+	"database/sql"
+)
+var db *sql.DB
+func Connection(Dbpath string)*sql.DB{
+
+	Db,err := sql.Open("mysql",Dbpath)
+	
+	if err!=nil{
+		log.Fatalln(err)
+	}
+	return Db
+}
