8e55f306b40138b1a27ccd9abc37ac7b6412ab08
ChaitanyaAkula <krishchaituakula@gmail.com>
2019-08-10 19:18:42 +0530

    Create dbconnection.go

---
+++ dbconnection.go
@@ -0,0 +1,17 @@
+package dbconnection	
+import(	
+	"log"	
+	_"github.com/go-sql-driver/mysql"	
+
+ 	"database/sql"	
+)	
+var db *sql.DB	
+func Connection()*sql.DB{	
+
+ 	Db,err := sql.Open("mysql","gittyjobs:Ancons927@tcp(gittyjobs.c96w2gvk26br.us-west-2.rds.amazonaws.com:3306)/gittyjobs")	
+
+ 	if err!=nil{	
+		log.Fatalln(err)	
+	}	
+	return Db	
+}
