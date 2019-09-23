f1de2d77dee4bf3c768ffc83f416bc887dc2f91b
ChaitanyaAkula <krishchaituakula@gmail.com>
2019-09-23 22:24:55 +0530

    Update dbconnection.go

--- dbconnection.go
+++ dbconnection.go
@@ -8,7 +8,7 @@ import(
 var db *sql.DB	
 func Connection()*sql.DB{	
 
- 	Db,err := sql.Open("mysql","gittyjobs:Ancons927@tcp(database-1.crp5gpwzu2zf.us-east-2.rds.amazonaws.com:3306)/gittyjobs")	
+ 	Db,err := sql.Open("mysql","gittyjobs:Ancons927@tcp(gittyjobs.crp5gpwzu2zf.us-east-2.rds.amazonaws.com:3306)/gittyjobs")	
 
  	if err!=nil{	
 		log.Fatalln(err)	
